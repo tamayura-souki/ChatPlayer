d5a4d029ad22dde411fd3cf7989806926c207f8c
eteeeeeerminal <momentneet@gmail.com>
2019-12-20 22:18:04 +0900

    chat の取得部分差し替え

--- main.py
+++ main.py
@@ -6,40 +6,49 @@ import pygame
 from pygame.locals import *
 import sys
 import time
-import concurrent.futures
 
-from ChatGetter import Chat_getter
+from pytchat    import LiveChat
 from ChatPlayer import Chat_player
 
 import json
-import logging
 import traceback
+
+
+def command_get(data):
+    global player
+
+    for c in data.items:
+        player.command_process([c.author.name, c.message])
+        data.tick()
 
 setting  = json.load(open("setting.json", 'r'))
 
 DISPLAY_SIZE = tuple(setting["win_size"])
 BACK_COLOR   = tuple(setting["back_color"])
-URL          = setting["chat_url"]
-GET_PERIOD   = setting["chat_get_period"]
+CHAT_ID      = setting["chat_id"]
 
 print("complete loding json")
 try:
-    getter = Chat_getter(URL, driver_path=setting["chromedriver_path"])
+    getter = LiveChat(CHAT_ID, callback = command_get)
+
 except:
-    getter.quit()
     sys.exit()
     
 player = None
 
-print("boot chrome_driver")
-
 def main():
     global player, getter
 
-    pygame.init()
-    screen = pygame.display.set_mode(DISPLAY_SIZE)
-    pygame.display.set_caption("ChatPlayer")
-    clock = pygame.time.Clock()
+    try:
+        pygame.init()
+        screen = pygame.display.set_mode(DISPLAY_SIZE)
+        pygame.display.set_caption("ChatPlayer")
+        clock = pygame.time.Clock()
+
+    except:
+        print("Error Pygame initialize")
+        traceback.print_exc()
+        sys.exit(0)
 
     try:
         player = Chat_player(DISPLAY_SIZE)
@@ -47,48 +56,37 @@ def main():
     except:
         print("Error ChatPlayer")
         pygame.quit()
-        getter.quit()
-        sys.exit()
+        sys.exit(0)
 
-    def loop():
-        clock.tick(30)
-        screen.fill(BACK_COLOR)
+    print("boot chatplayer")
 
-        chat_renders = player.draw()
-        if (not chat_renders == None) and (not chat_renders == []):
-            [screen.blit(chat_render["chat"], chat_render["pos"]) for chat_render in chat_renders]
 
-        pygame.display.update()
+    while getter.is_alive():
+        try:
+            clock.tick(30)
+            screen.fill(BACK_COLOR)
+
+            chat_renders = player.draw()
+            if (not chat_renders == None) and (not chat_renders == []):
+                [screen.blit(chat_render["chat"], chat_render["pos"]) for chat_render in chat_renders]
+
+            pygame.display.update()
+
+        except:
+            traceback.print_exc()
+            sys.exit(0)
         
         # 終了処理
         for event in pygame.event.get():
             if event.type == QUIT:
                 pygame.quit()
-                getter.quit()
-                sys.exit()
+                getter.terminate()
+                sys.exit(0)
 
-    try:
-        while True:
-            loop()
-
-    except:
-        traceback.print_exc()
-        pygame.quit()
-        getter.quit()
-        sys.exit()
+    pygame.quit()
+    getter.terminate()
+    sys.exit(0)                
 
 
-def command_get():
-    global getter, player
-    while True:
-        [player.command_process(c) for c in getter.get_chats()]
-        time.sleep(GET_PERIOD)
-
-executor = concurrent.futures.ThreadPoolExecutor(max_workers=2)
-try:
-    executor.submit(command_get)
-    executor.submit(main)
-
-except:
-    getter.quit()
-    sys.exit()
+if __name__ == "__main__":
+    main()
