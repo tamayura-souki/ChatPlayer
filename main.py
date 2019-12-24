e53370f32805e017e153cd43c578b1b9197b9f62
eteeeeeerminal <momentneet@gmail.com>
2019-12-24 12:20:05 +0900

    起動時に、liveのidを入力するようにした。

--- main.py
+++ main.py
@@ -2,91 +2,133 @@
 
 # PyGameで配信に映すやつをつくる
 
+import sys
+import traceback
+import time
+import json
+
+import tkinter as tk
+
 import pygame
 from pygame.locals import *
-import sys
-import time
 
 from pytchat    import LiveChat
 from ChatPlayer import Chat_player
 
-import json
-import traceback
+class ChatPlayerApp():
+    def __init__(self, json_file):
+        self.json_file = json_file
+        self.setting   = json.load(open(json_file, 'r'))
 
+        self.make_livechat()
 
-def command_get(data):
-    global player
+        self.end_f     = False
 
-    for c in data.items:
-        player.command_process([c.author.name, c.message])
-        data.tick()
+    # callback func
+    def command_get(self, data):
+        for c in data.items:
+            self.player.command_process([c.author.name, c.message])
+            data.tick()
 
-setting  = json.load(open("setting.json", 'r'))
+    def setting_window(self):
+        root = tk.Tk()
+        root.title("配信のIDを入力")
+        root.geometry("200x50")
 
-DISPLAY_SIZE = tuple(setting["win_size"])
-BACK_COLOR   = tuple(setting["back_color"])
-CHAT_ID      = setting["chat_id"]
+        def ok():
+            self.setting["chat_id"] = txt.get()
+            root.destroy()
 
-print("complete loding json")
-try:
-    getter = LiveChat(CHAT_ID, callback = command_get)
+        def end():
+            self.end_f = True
+            root.destroy()
+            sys.exit(0)
 
-except:
-    sys.exit()
-    
-player = None
+        lbl = tk.Label(text='ID')
+        lbl.place(x=10, y=10)
 
-def main():
-    global player, getter
+        txt = tk.Entry()
+        txt.place(x=30, y=10)
+        txt.insert(tk.END, self.setting["chat_id"])
 
-    try:
-        pygame.init()
-        screen = pygame.display.set_mode(DISPLAY_SIZE)
-        pygame.display.set_caption("ChatPlayer")
-        clock = pygame.time.Clock()
+        enter_btn = tk.Button(text="Ok", command=ok)
+        enter_btn.place(x=160, y=5)
 
-    except:
-        print("Error Pygame initialize")
-        traceback.print_exc()
-        sys.exit(0)
+        root.protocol("WM_DELETE_WINDOW", end)
+        root.mainloop()
 
-    try:
-        player = Chat_player(DISPLAY_SIZE)
-
-    except:
-        print("Error ChatPlayer")
-        pygame.quit()
-        sys.exit(0)
-
-    print("boot chatplayer")
-
-
-    while getter.is_alive():
+    def make_livechat(self):
         try:
-            clock.tick(30)
-            screen.fill(BACK_COLOR)
+            self.setting_window()
+            self.getter = LiveChat(self.setting["chat_id"], callback = self.command_get)
 
-            chat_renders = player.draw()
-            if (not chat_renders == None) and (not chat_renders == []):
-                [screen.blit(chat_render["chat"], chat_render["pos"]) for chat_render in chat_renders]
-
-            pygame.display.update()
+            with open(self.json_file, "w") as f:
+                json.dump(self.setting, f)
 
         except:
+            print("Error Live Chat")
+            sys.exit(-1)    
+
+    def init_pygame(self):
+        try:
+            pygame.init()
+            self.screen = pygame.display.set_mode(self.setting["win_size"])
+            pygame.display.set_caption("ChatPlayer")
+            self.clock = pygame.time.Clock()
+
+        except:
+            print("Error Pygame initialize")
             traceback.print_exc()
-            sys.exit(0)
+            sys.exit(-1)
+
+
+    def main(self):
         
-        # 終了処理
-        for event in pygame.event.get():
-            if event.type == QUIT:
+        self.init_pygame()
+
+        try:
+            self.player = Chat_player(self.setting["win_size"])
+
+        except:
+            print("Error ChatPlayer")
+            pygame.quit()
+            sys.exit(-1)
+
+        print("boot chatplayer")
+
+        while not self.end_f:
+            if not self.getter.is_alive():
+                print("live is none")
                 pygame.quit()
-                getter.terminate()
-                sys.exit(0)
+                self.make_livechat()
+                self.init_pygame()
 
-    pygame.quit()
-    getter.terminate()
-    sys.exit(0)                
+            try:
+                self.clock.tick(30)
+                self.screen.fill(self.setting["back_color"])
 
+                chat_renders = self.player.draw()
+                if (not chat_renders == None) and (not chat_renders == []):
+                    [self.screen.blit(chat_render["chat"], chat_render["pos"]) for chat_render in chat_renders]
+
+                pygame.display.update()
+
+            except:
+                print("error mainloop")
+                traceback.print_exc()
+                sys.exit(-1)
+        
+            # 終了処理
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    self.getter.terminate()
+                    sys.exit(0)
+
+        pygame.quit()
+        self.getter.terminate()
+        sys.exit(0)
 
 if __name__ == "__main__":
-    main()
+    chatplayer = ChatPlayerApp("setting.json")
+    chatplayer.main()
